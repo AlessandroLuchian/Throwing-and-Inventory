9e70e6019518e4d21869eef4096001e418019f28
Cazaceanu Octavian <70035078+CazaceanuOctavian@users.noreply.github.com>
2024-02-24 19:29:50 +0200

    tidied up project

--- Assets/Scripts/shooting/shootingBehaviour.cs
+++ Assets/Scripts/shooting/shootingBehaviour.cs
@@ -11,9 +11,11 @@ public class shootingBehaviour : MonoBehaviour
         //raycast from this position going forward 
         if(Physics.Raycast(this.transform.position + new Vector3(0, 1f, 0), this.transform.forward, out hit)) {
             Debug.Log(hit.transform.name);
-            //vfx
+            //vfx   
+            //in loc de new vector de aici va trebui pusa pozitia armei, la fel si pentru trail
+            ParticleSystem particleSystem = Instantiate(gunData.muzzleFlash, this.transform.position +  new Vector3(-1, 0, 0) , Quaternion.identity);
             TrailRenderer trail = Instantiate(gunData.bulletTrail, this.transform.position, Quaternion.identity);
-            StartCoroutine(calculateVfx(trail, hit));
+            StartCoroutine(calculateVfx_BulletTrail(trail, hit));
             target currentTarget = hit.transform.GetComponent<target>();
             if(currentTarget!=null) {
                 currentTarget.takeDamage(gunData.damage);
@@ -21,10 +23,11 @@ public class shootingBehaviour : MonoBehaviour
         }
     }
 
-    private IEnumerator calculateVfx(TrailRenderer currentTrail, RaycastHit hit) {
+    //posibile probleme daca inamicul se misca... In loc de Raycast HIT ar merge un vector3 cu pozitia din viitor a celui in care se trage
+    private IEnumerator calculateVfx_BulletTrail(TrailRenderer currentTrail, RaycastHit hit) {
         float time = 0;
         Vector3 startPosition = currentTrail.transform.position;
-
+        //godda go fast
         while (time < 1) {
             currentTrail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
             time += Time.deltaTime/currentTrail.time;
@@ -32,5 +35,6 @@ public class shootingBehaviour : MonoBehaviour
         }
         currentTrail.transform.position = hit.point;
         //instantiaza impactul aici dupa;
+        Destroy(currentTrail.gameObject, currentTrail.time);
     }
 }
